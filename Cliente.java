7b2e491aabd5de6e165f2d3c9d8f596ae9343db9
devansilva23 <104645346+devansilva23@users.noreply.github.com>
2022-10-22 00:22:02 -0300

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,29 @@
+package banco;
+
+public class Cliente {
+    private String nome;
+    private String cpf;
+    private String datadenascimento;
+    public Cliente(String nome, String cpf, String datadenascimento) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.datadenascimento = datadenascimento;
+        
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getDatadenascimento() {
+        return datadenascimento;
+    }
+
+}
