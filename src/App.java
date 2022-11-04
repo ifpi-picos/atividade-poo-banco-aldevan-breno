35bee41082c02baf94aa4312fc21b688bc639548
devansilva23 <104645346+devansilva23@users.noreply.github.com>
2022-11-04 16:49:08 -0300

    Delete src directory

--- src/App.java
+++
@@ -1,9 +0,0 @@
-package banco;
-
-import java.util.concurrent.CompletableFuture;
-
-public class App {
-    Cliente c1 = new Cliente("dan", "03221886648", "02031195");
-    Conta c2 = new Conta(0215, 0125444, 1000000, null);
-    Eendereco c3 = new Eendereco(null, null, 0, null, null, null);
-}
